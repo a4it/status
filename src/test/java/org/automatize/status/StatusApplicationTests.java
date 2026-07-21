77ef4359a7a0de674c344a3df6c7be80aba0cdc2
tim <tim@a4it.be>
2026-07-21 13:26:36 +0200

    Update project version to 0.0.105-SNAPSHOT and fix test class name

--- src/test/java/org/automatize/status/StatusApplicationTests.java
+++ src/test/java/org/automatize/status/StatusApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class clauStatusApplicationTests {
+class StatusApplicationTests {
 
     @Test
     void contextLoads() {
